28ff0f174db27f6f502ee59b1f6ef8edd5332721
seoulstice <jungyoungjoon@gmail.com>
2017-09-04 13:30:30 -0600

    Wrote more tests for ComputerAI class

--- test/computer_ai_test.rb
+++ test/computer_ai_test.rb
@@ -49,5 +49,29 @@ class ComputerAITest < Minitest::Test
     assert_equal 3, brain.submarine.length
   end
 
+  def test_brain_can_validate_both_ships_do_not_overlap
+    brain = ComputerAI.new
+    brain.create_destroyer
+    brain.create_submarine
+    combination = brain.destroyer & brain.submarine
 
+    if combination.empty?
+      assert brain.ship_placement_validation
+    else
+      refute brain.ship_placement_validation
+    end
+  end
+
+  def test_brain_can_create_ships
+    brain = ComputerAI.new
+    brain.create_ships
+
+    assert brain.ship_placement_validation
+    assert_equal 2, brain.destroyer.length
+    assert_equal 3, brain.submarine.length
+  end
+
+  def test
+
+  end
 end
